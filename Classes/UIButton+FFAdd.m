1f9d49bdf4e637e2a5b31ca8f3d92faf1d5cc677
XF_MBP <XF_MBP@qq.com>
2019-05-16 20:01:38 +0800

    UIButton+FFAdd

---
+++ Classes/UIButton+FFAdd.m
@@ -0,0 +1,66 @@
+//
+//  UIButton+FFAdd.m
+//  111
+//
+//  Created by 肖凡 on 2019/5/16.
+//  Copyright © 2019年 xiaofan. All rights reserved.
+//
+
+#import "UIButton+FFAdd.h"
+
+@implementation UIButton (FFAdd)
+
+-(void)layoutButtonWithImageAlignmentStyle:(FFButtonImageAlignmentStyle)style imageTitleSpace:(CGFloat)space{
+    // 1. 得到imageView和titleLabel的宽、高
+    CGFloat imageWith = self.imageView.frame.size.width;
+    CGFloat imageHeight = self.imageView.frame.size.height;
+    
+    CGFloat labelWidth = 0.0;
+    CGFloat labelHeight = 0.0;
+    if ([UIDevice currentDevice].systemVersion.floatValue >= 8.0) {
+        // 由于iOS8中titleLabel的size为0，用下面的这种设置
+        labelWidth = self.titleLabel.intrinsicContentSize.width;
+        labelHeight = self.titleLabel.intrinsicContentSize.height;
+    } else {
+        labelWidth = self.titleLabel.frame.size.width;
+        labelHeight = self.titleLabel.frame.size.height;
+    }
+    
+    // 2. 声明全局的imageEdgeInsets和labelEdgeInsets
+    UIEdgeInsets imageEdgeInsets = UIEdgeInsetsZero;
+    UIEdgeInsets labelEdgeInsets = UIEdgeInsetsZero;
+    
+    // 3. 根据style和space得到imageEdgeInsets和labelEdgeInsets的值
+    switch (style) {
+        case FFButtonImageAlignmentStyleTop:
+        {
+            imageEdgeInsets = UIEdgeInsetsMake(-labelHeight-space/2.0, 0, 0, -labelWidth);
+            labelEdgeInsets = UIEdgeInsetsMake(0, -imageWith, -imageHeight-space/2.0, 0);
+        }
+            break;
+        case FFButtonImageAlignmentStyleLeft:
+        {
+            imageEdgeInsets = UIEdgeInsetsMake(0, -space/2.0, 0, space/2.0);
+            labelEdgeInsets = UIEdgeInsetsMake(0, space/2.0, 0, -space/2.0);
+        }
+            break;
+        case FFButtonImageAlignmentStyleBottom:
+        {
+            imageEdgeInsets = UIEdgeInsetsMake(0, 0, -labelHeight-space/2.0, -labelWidth);
+            labelEdgeInsets = UIEdgeInsetsMake(-imageHeight-space/2.0, -imageWith, 0, 0);
+        }
+            break;
+        case FFButtonImageAlignmentStyleRight:
+        {
+            imageEdgeInsets = UIEdgeInsetsMake(0, labelWidth+space/2.0, 0, -labelWidth-space/2.0);
+            labelEdgeInsets = UIEdgeInsetsMake(0, -imageWith-space/2.0, 0, imageWith+space/2.0);
+        }
+            break;
+    }
+    
+    // 4. 赋值
+    self.titleEdgeInsets = labelEdgeInsets;
+    self.imageEdgeInsets = imageEdgeInsets;
+}
+
+@end

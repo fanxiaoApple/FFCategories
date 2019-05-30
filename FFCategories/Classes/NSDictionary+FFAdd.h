1d8716e2212cb383b4892eea762dc0751bb20a85
XF_MBP <XF_MBP@qq.com>
2019-05-30 19:54:52 +0800

    xx

--- FFCategories/Classes/NSDictionary+FFAdd.h
+++ FFCategories/Classes/NSDictionary+FFAdd.h
@@ -12,6 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSDictionary (FFAdd)
 - (NSString *)ff_JSONString;
++ (NSDictionary *)ff_dictionaryWithJSONString:(NSString *)jsonString;
 @end
 
 NS_ASSUME_NONNULL_END
